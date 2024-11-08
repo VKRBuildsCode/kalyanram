082a20b769301896ed7b8c7934b77f9a992273bc
9kalyan <rakalyanram@gmail.com>
2024-11-08 15:15:48 +0530

    maneesh

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="kalyanram",
+    version="0.0.3",
+    author="v kalyan ram",
+    author_email="vrchinnarathod@gmail.com",
+    url="",
+    description="An application that informs you of the time in different locations and timezones",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
