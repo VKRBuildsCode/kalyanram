0698104a8fbe4491f459f71bd9934500c0c93568
9kalyan <rakalyanram@gmail.com>
2024-11-08 15:52:15 +0530

    lkk

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from setuptools import setup, find_packages
 
-with open("README.rst", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 setup(
     name="kalyanram",
     version="0.0.8",
     author="V Kalyan Ram",
     author_email="vrchinnarathod@gmail.com",
     description="A brief description of the kalyanram package.",
-    long_description=long_description,
+    long_description=open("README.rst", "r", encoding="utf-8").read(),
     long_description_content_type="text/x-rst",  # Specify that the long description is in reStructuredText
     url="",  # Add the URL for the project or repository if available
     packages=find_packages(),  # Automatically find packages in the directory
